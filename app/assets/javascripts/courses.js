e081112c8952a9ddf53f06705a26e0519f790052
David Ladowitz <david@ladowitz.com>
2013-08-21 21:24:22 -0700

    testing broken Javascript shit 6

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,59 +1,59 @@
-// // Place all the behaviors and hooks related to the matching controller here.
-// // All this logic will automatically be available in application.js.
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
 
-// $(document).ready(function() {
-//   // alert('yes')
+$(document).ready(function() {
+  // alert('yes')
 
-// //Autocomplete for search box on homepage. TODO: Hook up some kind of search with results
-//   $(function() {
-//     $( ".search_box" ).autocomplete({
-//       source: availableSports
-//     });
-//   });
+//Autocomplete for search box on homepage. TODO: Hook up some kind of search with results
+  $(function() {
+    $( ".search_box" ).autocomplete({
+      source: availableSports
+    });
+  });
 
-// //cycles though images on course show page
-//   $('.main_image_section').cycle({
-//     fx:      'scrollRight',
-//     next:   '.arrow',
-//     timeout:  0,
-//     speed: 1500,
-//     easing:  'easeInOutBack',
-//   });
+//cycles though images on course show page
+  $('.main_image_section').cycle({
+    fx:      'scrollRight',
+    next:   '.arrow',
+    timeout:  0,
+    speed: 1500,
+    easing:  'easeInOutBack',
+  });
 
-// //set main image sizes
-//   // console.log($('.main .main_image img').width())
-//   // if( $('.main .main_image img').width() > 400 ){
-//   //   $('.main .course_content .main_image img').css('width', '100% ')
-//   // }
+//set main image sizes
+  // console.log($('.main .main_image img').width())
+  // if( $('.main .main_image img').width() > 400 ){
+  //   $('.main .course_content .main_image img').css('width', '100% ')
+  // }
 
-// //sets company logo size
-//   var logoHeight = $('.company img').height()
-//   var logoWidth = $('.company img').width()
+//sets company logo size
+  var logoHeight = $('.company img').height()
+  var logoWidth = $('.company img').width()
 
-//   $('.company .image').css({height: logoHeight, width: logoWidth})
+  $('.company .image').css({height: logoHeight, width: logoWidth})
 
-// //datepicker for start and end dates
-// $('#course_start_date').datepicker();
-// $('#course_end_date').timepicker();
+//datepicker for start and end dates
+$('#course_start_date').datepicker();
+$('#course_end_date').timepicker();
 
-// //Sports for search bar
-// var availableSports = [
-//   "Auto Racing",
-//   "Base Jumping",
-//   "Bull Riding",
-//   "Canyoneering",
-//   "Cave Diving",
-//   "Cliff Diving",
-//   "Hang Gliding",
-//   "Kayaking",
-//   "Motocross"];
-//   "Mountain Biking",
-//   "Mountaineering",
-//   "Motorcyle Racing",
-//   "Paragliging",
-//   "Rock Climbing",
-//   "Sailing",
-//   "Scuba Diving",
-//   "Skydiving",
-//   "White Water Rafting",
-// });
+//Sports for search bar
+var availableSports = [
+  "Auto Racing",
+  "Base Jumping",
+  "Bull Riding",
+  "Canyoneering",
+  "Cave Diving",
+  "Cliff Diving",
+  "Hang Gliding",
+  "Kayaking",
+  "Motocross"];
+  "Mountain Biking",
+  "Mountaineering",
+  "Motorcyle Racing",
+  "Paragliging",
+  "Rock Climbing",
+  "Sailing",
+  "Scuba Diving",
+  "Skydiving",
+  "White Water Rafting"
+});
