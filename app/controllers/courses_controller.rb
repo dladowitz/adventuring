7e2813b8ab33f786d99c480a0d023da01466c8ca
David Ladowitz <david@ladowitz.com>
2013-09-30 20:56:02 -0700

    finished Search by location and categorygit add .git add .

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,12 +8,23 @@ class CoursesController < ApplicationController
   def index
     # @courses = Course.paginate(:per_page => 15, :page => params[:page])
     # @courses = get_courses_nearby(location = 'Saratoga, CA, USA', distance_in_mi = 60)
-    if params[:location]
-      #if we get a location we can look for courses nearby. This doesn't use the full text search currently. Need to add in.
-      @courses = get_courses_nearby(params[:location], 60).paginate(:page => params[:page], :per_page => 9)
-    else
-      #if we don't get a location.
+
+    #used by index page before search form is submitted
+    params[:location] = 'Anywhere' unless params[:location]
+
+    #from search form
+    #if we don't get a location.
+    if params[:location] == 'Anywhere'
       @courses = Course.text_search(params[:query]).paginate(:page => params[:page], :per_page => 9)
+
+    #if we get a location and a query
+    elsif params[:query]
+      courses_from_search_query = Course.text_search(params[:query])
+      @courses = filter_courses_by_location(params[:location], 60, courses_from_search_query).paginate(:page => params[:page], :per_page => 9)
+
+    #if we get a location, but no query
+    else
+      @courses = get_courses_nearby(params[:location], 60).paginate(:page => params[:page], :per_page => 9)
     end
   end
 
@@ -67,18 +78,37 @@ class CoursesController < ApplicationController
     @review       = Review.new
   end
 
-  private
-    def get_courses_nearby(location = nil, distance_in_mi = nil)
-    if location && distance_in_mi
-      sections = Section.near(location, distance_in_mi)
-      courses = []
-      sections.each do |section|
-        courses << section.course
+  # private
+    def get_courses_nearby(location, distance_in_mi)
+      if location && distance_in_mi
+        sections = Section.near(location, distance_in_mi)
+        courses = []
+        sections.each do |section|
+          courses << section.course
+        end
+        courses
       end
-      courses
-    else
-      Course.all
     end
-  end
 
+    def filter_courses_by_location(location, distance_in_mi, courses)
+      puts "starting method"
+      puts "number of courses: #{courses.count}"
+      filtered_courses = []
+
+      courses.each do |course|
+        puts "course name: #{course.name}"
+        course.sections.each do |section|
+          puts "section location: #{section.location}"
+          distance_to_location = section.distance_to(location)
+
+          puts "section.distance_to(location): #{distance_to_location}"
+
+          if distance_to_location && distance_to_location < distance_in_mi
+            filtered_courses << section.course
+          end
+        end
+      end
+
+      filtered_courses.uniq
+    end
 end
