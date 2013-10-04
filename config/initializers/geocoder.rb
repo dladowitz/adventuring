75e59db7d33540ad9d03fdfc1a5b5e6b72fadbe9
David Ladowitz <david@ladowitz.com>
2013-10-04 16:02:18 -0700

    added note to geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,4 @@
+#used because of the 2500 limit on geocoder. Google tracks usage by IP address. On Heroku the IP is shared and the limit is exceeded by others
 Geocoder.configure(
   :http_proxy => 'quotaguard859:Az756bG4fTTx@QuotaGuard-1586506616.us-east-1.elb.amazonaws.com:9292',
   :timeout => 5
